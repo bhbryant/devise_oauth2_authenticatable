81094d8bb0b4f5ae6d5897a58b30aaf7fa4f918c
Jerry Luk <jerryluk@gmail.com>
2010-06-28 16:26:33 -0700

    Fixed  a bug where error is generated when oauth_config.yml is not generated

--- lib/devise_oauth2_authenticatable/rails.rb
+++ lib/devise_oauth2_authenticatable/rails.rb
@@ -1,7 +1,11 @@
 module Devise
   class Engine
     config.after_initialize do
-      Devise::OAUTH2_CONFIG = YAML.load_file(Rails.root.join('config', 'oauth2_config.yml'))[Rails.env]
+      begin
+        Devise::OAUTH2_CONFIG = YAML.load_file(Rails.root.join('config', 'oauth2_config.yml'))[Rails.env]
+      rescue
+        puts "Can't load oauth2_config.yml. You can generate with rails g devise:oauth2_authenticatable"
+      end
     end
   end
 end
