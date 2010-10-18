2597cb326660fe685ba41efa732dd6d69b3566cc
Kevin Ball <kmball11@gmail.com>
2010-10-18 11:15:37 -0700

    Devise already adds scoping; we don't need to

--- lib/devise_oauth2_authenticatable/routes.rb
+++ lib/devise_oauth2_authenticatable/routes.rb
@@ -2,8 +2,6 @@ ActionDispatch::Routing::Mapper.class_eval do
   protected
   
   def devise_oauth2(mapping, controllers)
-    scope mapping.path do
-      get mapping.path_names[:oauth2],  :to => "#{controllers[:sessions]}#create", :as => :"oauth2_#{mapping.name}_session"
-    end
+    get mapping.path_names[:oauth2],  :to => "#{controllers[:sessions]}#create", :as => :"oauth2_#{mapping.name}_session"
   end
 end
